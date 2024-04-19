617968d9041904e2fe4a73b64e1c5bca8d5225f3
Viorel <eremiaviorel0@gmail.com>
2024-04-19 15:29:37 +0300

    Refactoring docker containers

---
+++ app/src/Entity/Quote.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\QuoteRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: QuoteRepository::class)]
+class Quote
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $quote = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $string = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getQuote(): ?string
+    {
+        return $this->quote;
+    }
+
+    public function setQuote(string $quote): static
+    {
+        $this->quote = $quote;
+
+        return $this;
+    }
+
+    public function getString(): ?string
+    {
+        return $this->string;
+    }
+
+    public function setString(string $string): static
+    {
+        $this->string = $string;
+
+        return $this;
+    }
+}
